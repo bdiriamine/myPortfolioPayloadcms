7d8b7668432f76f298756d0a784dfc0cf1565fd9
mohamed amine bdiri <bdiri.amine08@gmail.com>
2025-05-26 22:27:31 +0100

    fix height

--- src/app/(frontend)/[slug]/page.tsx
+++ src/app/(frontend)/[slug]/page.tsx
@@ -79,7 +79,7 @@ export default async function Page({ params: paramsPromise }: Args) {
           <Image
             src={getImageUrl(heroImage, '')}
             alt="slider"
-            className="w-full h-auto object-cover"
+            className="w-full h-auto md:h-[200px] object-cover"
           />
         </div>
       )}
